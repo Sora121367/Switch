3b43d8f2ffec239b49d532fd25553ef5adcf5430
LuckyHeng9 <123loemheng@gmail.com>
2024-11-20 19:17:04 +0700

    code card new

--- my-react-app/src/component/HomePage.jsx
+++ my-react-app/src/component/HomePage.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import bannerImage from "../assets/banner.png";
 import CategoryCard from "./CategoryCard";
-
+import StoreCard from "./StoreCard";
 
 const HomePage = () => {
   return (
     <div className="relative w-full ">
-
-
       <div className="relative w-full">
         <img
           src={bannerImage}
@@ -18,7 +16,8 @@ const HomePage = () => {
         {/* Text and Button on Banner */}
         <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4">
           <h1 className="text-white font-bold text-xl sm:text-3xl lg:text-4xl  leading-tight">
-            Explore a World of Unique Shops and <br />Products Tailored Just for You!
+            Explore a World of Unique Shops and <br />
+            Products Tailored Just for You!
           </h1>
           <button
             className="mt-3 p-3 text-white bg-[#0B5754] rounded-md text-sm sm:text-md lg:text-xl lg:mt-16  hover:bg-blue-700 transition-all duration-300"
@@ -29,16 +28,19 @@ const HomePage = () => {
         </div>
       </div>
 
-    {/* Feature and catategqries */}
+      {/* Feature and catategqries */}
 
-    <div className="mt-20 px-20 py-4 bg-red-300 w-full h-[100vh]">
-      <h1 className="text-2xl font-bold">Feature and catategqries </h1>
-     
-      <div>
-      <CategoryCard/>
+      <div className="mt-20 px-20 py-4 bg-red-300 w-full h-[100vh]">
+        <h1 className="text-2xl font-bold">Feature and catategqries </h1>
+
+        <div className="flex space-x-10">
+          <CategoryCard/>
+          <StoreCard/>
+        </div>
+      
+          
+        
       </div>
-    </div>
-
     </div>
   );
 };
